1f9e43ae5faec8cbfd71aea8b6e6bda5ef5d062d
lihbr <lihbr@users.noreply.github.com>
2021-11-15 11:31:42 +0100

    fix: accomodate @nuxt/modules new structure

--- src/module.js
+++ src/module.js
@@ -5,9 +5,7 @@ export default async function moduleDocs() {
 	// 1. Get all modules manifests from modules.nuxtjs.org
 	let allModuleManifests;
 	try {
-		const res = await fetch(
-			"https://unpkg.com/@nuxt/modules/dist/modules.json",
-		);
+		const res = await fetch("https://unpkg.com/@nuxt/modules/modules.json");
 		if (!res.ok) {
 			throw res;
 		}
@@ -40,7 +38,7 @@ export default async function moduleDocs() {
 				lines.push(
 					`${chalk.yellow(manifest.npm)} (${
 						manifest.category
-					}): ${chalk.cyanBright(manifest.website)}`,
+					}): ${chalk.cyanBright(manifest.website)}`
 				);
 			});
 		}
